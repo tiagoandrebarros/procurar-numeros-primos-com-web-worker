38931ec2eb0c92dd4e4f9efb60e70d2e328d2efb
Tiago Barros <97137396+tiagobarrossilva@users.noreply.github.com>
2023-10-14 01:07:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const inicio = document.querySelector('#inicio')
+const fim = document.querySelector('#fim')
+const situacao = document.querySelector('#situacao')
+const telaResultado = document.querySelector('#telaResultado')
+
+function calcular() {
+  situacao.textContent = 'Processando...'
+  
+  // instanciando worker em nova thread
+  const worker = new Worker("worker.js")
+
+  // enviando menssagem para o worker
+  worker.postMessage([inicio.value, fim.value])
+
+  // ..........
+
+  // ouvindo, aguardando resposta do worker
+  worker.onmessage = function (e) {
+    
+    // fazendo processamento após receber a resposta do worker
+    telaResultado.textContent = ''
+
+    if(e.data.length == 0){
+      situacao.textContent = 'Não foi encontrado algum número primo'
+    } else if(e.data == false){
+      situacao.textContent = 'Aconteceu algum erro, verifique a digitação e tente novamente'
+    } else{
+      situacao.textContent = 'processamento concluído'
+
+      for(let i in e.data){
+        let container = document.getElementById("telaResultado")
+        let elemento = document.createElement("span")
+        elemento.appendChild(document.createTextNode(e.data[i]))
+        container.appendChild(elemento)
+      }
+    }
+    
+    // finalizando o worker/thread
+    worker.terminate()
+  }
+}
---
+++ worker.js
@@ -0,0 +1,41 @@
+onmessage = function (e) {
+  let inicio = e.data[0]
+  let fim = e.data[1]
+  
+  inicio = parseInt(inicio)
+  fim = parseInt(fim)
+  
+  if(Number.isNaN(inicio) || Number.isNaN(fim)){
+    postMessage(false)
+    return
+  }
+  
+  let listaResultado = []
+
+  if(inicio > fim || inicio < 0 || fim < 0){
+    postMessage(false)
+  } else if(inicio === 0 || inicio === 1){
+    inicio = 2
+  }
+
+  while(inicio <= fim){
+    let i = 2
+    let primo = true
+
+    while(i < inicio){
+      if(inicio % i === 0){
+        primo = false
+        break
+      }
+      i++
+    }
+
+    if(primo){
+      listaResultado.push(inicio)
+    }
+
+    inicio++
+  }
+
+  postMessage(listaResultado)
+}
